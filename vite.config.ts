dff3d0cb6bf303935eb809a4f0fab43f07f5f7c4
diegosan52 <diegosan35@gmail.com>
2026-02-09 18:41:38 -0500

    fix: use relative base path for better github pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LANDIN-PROYECTO-INSPIRANDO-PADRES/',
+  base: './',
 })
